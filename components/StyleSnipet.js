02607dfc0a9fbe64ec6df940b4a2b6dc58af47c2
calebbenjin <calebben94@gmail.com>
2021-07-23 16:12:26 +0100

    styled-components-configuration

---
+++ components/StyleSnipet.js
@@ -0,0 +1,44 @@
+import styled, {css} from 'styled-components'
+import { useState } from 'react';
+
+
+const Body = styled.div`
+  text-align: center;
+  margin: 5rem 0;
+`;
+
+const Title = styled.h1`
+  /* color: ${props => props.color ? props.color : 'red'} */
+  ${props => props.primary && css`
+    color: red;
+    font-size: 3rem;
+  `}
+`;
+
+const Button = styled.button`
+  font-size: 2rem;
+  background: #333;
+  padding: 10px 30px;
+  color: #fff;
+  border: none;
+  border-radius: 55px;
+  background: ${props => props.bg ? props.bg : '#333'};
+  width: ${props => props.maxWidth ? props.maxWidth : ''};
+  transition: ease 0.2s all;
+  ${props => props.primary && css`
+    background: ${props => props.theme.primary.baseColor};
+    font-size: 3rem;
+  `}
+`;
+
+
+export default function StyleSnipet({children}) {
+  const [primary, setPrimary] = useState(false)
+  return (
+    <Body>
+      <Title >Mastering styled components</Title>
+      <Button bg="red" primary={primary} onClick={() => setPrimary(!primary)}>Click me</Button>
+      {children}
+    </Body>
+  )
+}
---
+++ styles/theme.js
@@ -0,0 +1,22 @@
+
+
+export default {
+  fonts:{},
+  colors:{},
+  breakpoints: {
+    mobile: "only screen and (max-width: 50rem)",
+    tablet: "only screen and (max-width: 65rem)"
+  },
+  spacing: {
+    xxSmall: ".25rem",
+    xSmall: ".5rem",
+    small: "1rem",
+    medium: "2rem",
+    large: "3rem",
+    xlarge: "4rem",
+    xxlarge: "6rem",
+  },
+  shadows: {
+    shadow1: "0px 5px 20px rgba(30, 30, 31, 0.05)"
+  }
+}

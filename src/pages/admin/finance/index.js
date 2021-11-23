d01e8300e38497f3439b26834e6b5b5f7520b4d5
calebbenjin <calebben94@gmail.com>
2021-11-23 13:34:25 +0100

    Finance-page resolved

--- src/pages/admin/finance/index.js
+++ src/pages/admin/finance/index.js
@@ -1,14 +1,14 @@
-import Container from "src/components/atoms/Container";
-import Table from "src/components/atoms/Table";
-import Heading from "../../../components/atoms/Heading";
-import Layout from "../../../components/organisms/Layout";
+import Container from "@/components/atoms/Container";
+import Table from "@/components/atoms/Table";
+import Heading from "@/components/atoms/Heading";
+import Layout from "@/components/organisms/Layout";
 
 export default function FinancePage() {
   return (
     <Layout >
       <Container>
         <Heading title="Finance Page" />
-        <Table />
+        {/* <Table /> */}
       </Container>
     </Layout>
   )

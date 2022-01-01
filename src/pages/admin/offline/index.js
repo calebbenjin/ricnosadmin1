69b6772d385440fedeae3931420280b5ef251349
calebbenjin <calebben94@gmail.com>
2022-01-01 09:18:41 +0100

    Offline-form changes

--- src/pages/admin/offline/index.js
+++ src/pages/admin/offline/index.js
@@ -3,111 +3,125 @@ import Header from '@/components/atoms/Heading'
 import Layout from '@/components/organisms/Layout'
 import { Button, Grid, Heading, Text, FormControl, Select, Input } from '@chakra-ui/react'
 import { FaListUl } from 'react-icons/fa'
-import Logo from '@/components/atoms/Logo'
 
-export default function OfflineOrderPage() {
+export default function OfflinePage() {
 
+  const handleSubmit = () => {
+    console.log("Hello world")
+  }
   
   return (
     <Layout>
       <Container>
         <Header title="My Orders" icon={<FaListUl />} />
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <Heading size="sm" mb="4">Personal Data</Heading>
-          <Grid gridTemplateColumns='repeat(3, 1fr)' gap="5">
-            <FormControl>
-              <Input type="text" id="name" id="name" name="name" placeholder="Name" />
-            </FormControl>
-            <FormControl>
-              <Input type="email" id="email" id="email" name="email" placeholder="Email" />
-            </FormControl>
-            <FormControl>
-              <Input type="text" id="phone" id="phone" name="phone" placeholder="Phone Number" />
-            </FormControl>
-          </Grid>
+          <Box>
+            <Grid gridTemplateColumns='repeat(3, 1fr)' gap="5">
+              <FormControl>
+                <Input type="text" id="name" id="name" name="name" placeholder="Name" />
+              </FormControl>
+              <FormControl>
+                <Input type="email" id="email" id="email" name="email" placeholder="Email" />
+              </FormControl>
+              <FormControl>
+                <Input type="text" id="phone" id="phone" name="phone" placeholder="Phone Number" />
+              </FormControl>
+            </Grid>
+          </Box>
           <FormControl mt="4">
             <Input type="text" id="address" id="address" name="address" placeholder="Address" />
           </FormControl>
 
           <Heading size="sm" my="4">Shippment Data</Heading>
-          <Grid gridTemplateColumns='repeat(3, 1fr)' gap="5">
-            <FormControl>
-              <Input type="text" id="recieversName" id="recieversName" name="recieversName" placeholder="Recievers Name" />
-            </FormControl>
-            <FormControl>
-              <Input type="text" id="recieversphone" id="recieversphone" name="recieversphone" placeholder="Phone Number" />
-            </FormControl>
-            <FormControl>
-              <Input type="email" id="recieversemail" id="recieversemail" name="recieversemail" placeholder="Email" />
-            </FormControl>
-          </Grid>
-          <Grid gridTemplateColumns='repeat(3, 1fr)' gap="5" mt="4">
-            <FormControl>
-              <Input type="text" id="description" id="description" name="description" placeholder="Description" />
-            </FormControl>
-            <FormControl>
-              <Input type="text" id="departure" id="departure" name="departure" placeholder="Departure" />
-            </FormControl>
-            <FormControl>
-              <Input type="text" id="arrival" id="arrival" name="arrival" placeholder="Arrival" />
-            </FormControl>
-          </Grid>
+          <Box>
+            <Grid gridTemplateColumns='repeat(3, 1fr)' gap="5">
+              <FormControl>
+                <Input type="text" id="recieversName" id="recieversName" name="recieversName" placeholder="Recievers Name" />
+              </FormControl>
+              <FormControl>
+                <Input type="text" id="recieversphone" id="recieversphone" name="recieversphone" placeholder="Phone Number" />
+              </FormControl>
+              <FormControl>
+                <Input type="email" id="recieversemail" id="recieversemail" name="recieversemail" placeholder="Email" />
+              </FormControl>
+            </Grid>
+          </Box>
+          <Box>
+            <Grid gridTemplateColumns='repeat(3, 1fr)' gap="5" mt="4">
+              <FormControl>
+                <Input type="text" id="description" id="description" name="description" placeholder="Description" />
+              </FormControl>
+              <FormControl>
+                <Input type="text" id="departure" id="departure" name="departure" placeholder="Departure" />
+              </FormControl>
+              <FormControl>
+                <Input type="text" id="arrival" id="arrival" name="arrival" placeholder="Arrival" />
+              </FormControl>
+            </Grid>
+          </Box>
           <FormControl mt="4">
             <Input type="text" id="address" id="address" name="address" placeholder="Address" />
           </FormControl>
-          <Grid gridTemplateColumns='repeat(4, 1fr)' gap="5" mt="4">
-            <FormControl>
-              <Input type="text" id="city" id="city" name="city" placeholder="City" />
-            </FormControl>
-            <FormControl>
-              <Input type="text" id="state" id="state" name="state" placeholder="State" />
-            </FormControl>
-            <FormControl>
-              <Select placeholder='Select Region'>
-                <option value='option1'>Option 1</option>
-                <option value='option2'>Option 2</option>
-                <option value='option3'>Option 3</option>
-              </Select>
-            </FormControl>
-            <FormControl>
-              <Select placeholder='Select Rider'>
-                <option value='option1'>Option 1</option>
-                <option value='option2'>Option 2</option>
-                <option value='option3'>Option 3</option>
-              </Select>
-            </FormControl>
-          </Grid>
+          <Box>
+            <Grid gridTemplateColumns='repeat(4, 1fr)' gap="5" mt="4">
+              <FormControl>
+                <Input type="text" id="city" id="city" name="city" placeholder="City" />
+              </FormControl>
+              <FormControl>
+                <Input type="text" id="state" id="state" name="state" placeholder="State" />
+              </FormControl>
+              <FormControl>
+                <Select placeholder='Select Region'>
+                  <option value='option1'>Option 1</option>
+                  <option value='option2'>Option 2</option>
+                  <option value='option3'>Option 3</option>
+                </Select>
+              </FormControl>
+              <FormControl>
+                <Select placeholder='Select Rider'>
+                  <option value='option1'>Option 1</option>
+                  <option value='option2'>Option 2</option>
+                  <option value='option3'>Option 3</option>
+                </Select>
+              </FormControl>
+            </Grid>
+          </Box>
           <Heading size="sm" mt="10" mb="4">Items <sup>(item 1)</sup></Heading>
-          <Grid gridTemplateColumns='repeat(3, 1fr)' gap="5">
-            <FormControl>
-              <Input type="text" id="itemName" id="itemName" name="itemName" placeholder="Items Name" />
-            </FormControl>
-            <FormControl>
-              <Input type="text" id="quantity" id="quantity" name="quantity" placeholder="Quantity" />
-            </FormControl>
-            <FormControl>
-              <Input type="text" id="itemValue" id="itemValue" name="itemValue" placeholder="Item Value" />
-            </FormControl>
-          </Grid>
-          <Grid gridTemplateColumns='repeat(4, 1fr)' gap="5" mt="4">
-            <FormControl>
-              <Input type="text" id="amount" id="amount" name="amount" placeholder="Amount" />
-            </FormControl>
-            <FormControl>
-              <Select placeholder='Delivery Method'>
-                <option value='option1'>Option 1</option>
-                <option value='option2'>Option 2</option>
-                <option value='option3'>Option 3</option>
-              </Select>
-            </FormControl>
-            <FormControl>
-              <Input type="text" id="weight" id="weight" name="weight" placeholder="weight" />
-            </FormControl>
-            <FormControl>
-              <Input type="file" />
-            </FormControl>
-          </Grid>
+          <Box>
+            <Grid gridTemplateColumns='repeat(3, 1fr)' gap="5">
+              <FormControl>
+                <Input type="text" id="itemName" id="itemName" name="itemName" placeholder="Items Name" />
+              </FormControl>
+              <FormControl>
+                <Input type="text" id="quantity" id="quantity" name="quantity" placeholder="Quantity" />
+              </FormControl>
+              <FormControl>
+                <Input type="text" id="itemValue" id="itemValue" name="itemValue" placeholder="Item Value" />
+              </FormControl>
+            </Grid>
+          </Box>
+          <Box>
+            <Grid gridTemplateColumns='repeat(4, 1fr)' gap="5" mt="4">
+              <FormControl>
+                <Input type="text" id="amount" id="amount" name="amount" placeholder="Amount" />
+              </FormControl>
+              <FormControl>
+                <Select placeholder='Delivery Method'>
+                  <option value='option1'>Option 1</option>
+                  <option value='option2'>Option 2</option>
+                  <option value='option3'>Option 3</option>
+                </Select>
+              </FormControl>
+              <FormControl>
+                <Input type="text" id="weight" id="weight" name="weight" placeholder="weight" />
+              </FormControl>
+              <FormControl>
+                <Input type="file" />
+              </FormControl>
+            </Grid>
+          </Box>
 
           <Text my="10">Add more items</Text>
 

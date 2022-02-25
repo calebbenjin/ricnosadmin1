a82421c7f6dee79e59de8a5a1a0f483e6f391e92
calebbenjin <calebben94@gmail.com>
2022-02-25 13:19:47 +0100

    Accept application/json added

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -9,6 +9,7 @@ const login = async (req, res) => {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        'Accept': 'application/json'
       },
       body: JSON.stringify({
         email,

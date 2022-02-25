63c2154c0ed159b991ac46f7306751a3d2aa98dc
moses <ogbopinamoses@gmail.com>
2022-02-25 19:21:50 +0100

    bug added accept header to api/user

--- src/pages/api/user.js
+++ src/pages/api/user.js
@@ -1,21 +1,22 @@
-import { API_URL } from '@/lib/index';
-import cookie from 'cookie';
+import { API_URL } from "@/lib/index";
+import cookie from "cookie";
 
 const user = async (req, res) => {
-  if (req.method === 'GET') {
+  if (req.method === "GET") {
     if (!req.headers.cookie) {
-      res.status(403).json({ message: 'Not Authorized' });
+      res.status(403).json({ message: "Not Authorized" });
       return;
     }
 
-    const { token } = cookie.parse(req.headers.cookie)
+    const { token } = cookie.parse(req.headers.cookie);
 
-    console.log(token)
+    // console.log(token)
 
     const apiRes = await fetch(`${API_URL}`, {
-      method: 'GET',
+      method: "GET",
       headers: {
         Authorization: `Bearer ${token}`,
+        Accept: "application/json",
       },
     });
 
@@ -24,10 +25,10 @@ const user = async (req, res) => {
     if (apiRes.ok) {
       res.status(200).json({ user });
     } else {
-      res.status(403).json({ message: 'User forbidden' });
+      res.status(403).json({ message: "User forbidden" });
     }
   } else {
-    res.setHeader('Allow', ['POST']);
+    res.setHeader("Allow", ["POST"]);
     res.status(405).json({ message: `Method ${req.method} not allowed` });
   }
 };
